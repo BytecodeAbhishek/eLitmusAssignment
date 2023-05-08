ecf2df6664a3e68784e60d16d9abfa744d014f60
ByteCodeAbhishek <abhishek.chaudhary.dev@gmail.com>
2023-05-09 03:33:46 +0530

    Commit changes

--- js/end.js
+++ js/end.js
@@ -1,11 +1,16 @@
 let btn = document.getElementById('claimBtn');
+var button2 = document.getElementById("button2");
+button2.addEventListener("click", function() {
+    // Action to be performed when Button 2 is clicked
+    window.location.href = 'LeaderBoard.html';
+  });
 btn.onclick = function () {
     document.querySelector('img').src = 'images/img4.webp';
-
+    
     setTimeout(function () {
         firebase.auth().signOut().then(() => {
             alert('You have been signed out. Thank you for playing this game');
-            window.location.href = 'LeaderBoard.html';
+            window.location.href = 'index.html';
         }).catch((error) => {
 
         });
